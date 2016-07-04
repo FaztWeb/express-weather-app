eaa5f1003e12f71043c8816f7e4a44a79a7dc139
Fabian Jesus <fabianncdev@gmail.com>
2016-07-04 09:30:15 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const ForecastIo = require('forecastio');
+
+let app=express();
+let weather= new ForecastIo("609c705291760311f891bb4f266f6460");
+
+app.set('port',process.env.PORT||3000);
+app.set('x-powered-by',false);
+
+app.use(express.static(path.resolve(__dirname,'public')));
+app.set('views',path.resolve(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+  res.render('index');
+});
+
+app.get('/latitude/:latitude/longitude/:longitude',(req,res,next)=>{
+  console.log(req.params.latitude, req.params.longitude);
+  if (!req.params.latitude || !req.params.longitude) {
+    res.status(404).render("404");
+    return;
+  }
+  let latitude=parseInt(req.params.latitude,10);
+  let longitude=parseInt(req.params.longitude,10);
+  weather.forecast(latitude,longitude,(err,data)=>{
+    if (err) {
+      next();
+      return;
+    }
+    res.json({
+      temperature:data.currently.temperature,
+      timezone:data.timezone
+    });
+  });
+});
+
+app.use((req,res)=>{
+  res.status(404).render("404");
+});
+
+app.listen(3000);
---
+++ public/main.js
@@ -0,0 +1,46 @@
+$(function() {
+  var $h1=$("h1");
+  var $latitude=$("input[name='latitude']");
+  var $longitude=$("input[name='longitude']");
+  var btnLocation=$("input[name='btnLocation']");
+
+  btnLocation.on('click',obtener);
+
+  function obtener() {
+    geoconfig={
+      enableHighAccuracy:true,
+      timeout:10000,
+      maximunAge:60000
+    };
+    navigator.geolocation.getCurrentPosition(mostrar,errores,geoconfig);
+  }
+
+  function mostrar(position) {
+    $latitude.val(position.coords.latitude );
+    $longitude.val(position.coords.longitude);
+  }
+
+  function errores(error) {// este objeto tine dos atributos: error y message
+    alert('Error: '+error.code + ' '+error.message);
+  }
+
+  $('form').on('submit',function(event){
+    event.preventDefault();
+    var latitude=$.trim($latitude.val());
+    var longitude=$.trim($longitude.val());
+
+    $('h1').text('Loading...');
+
+    var req=$.ajax({
+      url:"/latitude/"+latitude+"/longitude/"+longitude,
+      dataType:'json'
+    });
+    req.done(function(data){
+      var temperature= data.temperature;
+      $h1.html("the temperature in "+ data.timezone+" is "+temperature+ "&#176 Fahrenheit; in latitude:"+latitude+" and longitude: "+longitude);
+    });
+    req.fail(function(){
+      $h1.text('Error!');
+    });
+  });
+});
